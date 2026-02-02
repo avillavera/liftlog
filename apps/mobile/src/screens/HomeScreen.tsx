8c034409a305dd1d38469bbe9d4274bac5cf273c
avillavera <avillavera@gmail.com>
2026-02-01 19:28:52 -0500

    feat: scaffold mobile app with navigation. Added RootNavigator.tsx, HomeScreen.tsx and modified App.tsx

---
+++ apps/mobile/src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>LiftLog</Text>
+      <Text>Mobile scaffold is running ✅</Text>
+    </View>
+  );
+}
