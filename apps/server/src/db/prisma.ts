6fb011bd5b5ca11175072203a27e01434c4a444c
avillavera <avillavera@gmail.com>
2026-02-06 01:25:15 -0500

    feat(server): added middleware to attach userId to requests if auth token is valid

--- apps/server/src/db/prisma.ts
+++ apps/server/src/db/prisma.ts
@@ -11,7 +11,8 @@ const adapter = new PrismaPg({ connectionString });
 // Avoid creating many clients in dev (hot reload)
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
---
+++ apps/server/src/middleware/requireAuth.ts
@@ -0,0 +1,25 @@
+import type { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../lib/jwt.js";
+
+//Custom type to add userId to request
+export type AuthedRequest = Request & { userId: string };
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  const header = req.headers.authorization;
+
+  //Authorization: Bearer <JWT_TOKEN_HERE>  
+  if (!header?.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Missing auth token" });
+  }
+
+  const token = header.slice("Bearer ".length).trim();
+
+  try {
+    const payload = verifyToken(token);
+    (req as AuthedRequest).userId = payload.userId;
+    next();
+  } 
+  catch {
+    return res.status(401).json({ error: "Invalid auth token" });
+  }
+}
